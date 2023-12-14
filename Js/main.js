14c124bee0cc6c4ea5a38926a5c77ce701fe77f3
yuransimao <yuransimao7@gmail.com>
2023-12-14 19:47:26 +0100

    adicionando funcionalidade

---
+++ Js/main.js
@@ -0,0 +1,53 @@
+const themesWorks = [
+    "Grupo Nº 1: Micro-controladores",
+    "Grupo Nº 2: Linguagem Assembly",
+    "Grupo Nº 3: Clock de um Computador",
+    "Grupo Nº 4: Gabinete",
+    "Grupo Nº 5: Fonte de alimentação",
+    "Grupo Nº 6: Placa mãe",
+    
+    "Grupo Nº 8: Armazenamento em massa",
+    "Grupo Nº 9: Gestão de memória"
+];
+
+let TheMeselected =[]
+
+const storeData = localStorage.getItem("Theme") ||[];
+
+function generateTheme (){
+    const themeContainer = document.getElementById("theme-container");
+    const n =themesWorks.length;
+    let   themeselect;
+
+
+    for(let i= n -1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1))
+        const temp = themesWorks[i];
+        themesWorks[i] = themesWorks[j];
+        themesWorks[j] = temp;
+              
+        
+    }
+    themeselect = themesWorks[0];
+    themeContainer.textContent= themeselect
+    if(TheMeselected.includes(themeselect)){
+        
+        TheMeselected.filter(item => item !== TheMeselected)
+    }
+    else{
+        TheMeselected.push(themeselect);
+        localStorage.setItem("Theme", JSON.stringify(TheMeselected) )
+
+        if(TheMeselected.length === themesWorks.length){
+            themeContainer.textContent= "Fim"
+        }
+    }
+    
+
+    
+
+}
+
+
+
+document.getElementById("button").addEventListener("click", generateTheme);
